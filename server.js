f37fbd0a8f1a1bb87d4989d86d13658943c2de7b
Mukesh Sahay <muksahay@amazon.com>
2023-06-04 08:11:26 -0700

    fix(json): updated path for json file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router("data.json");
+const router = jsonServer.router(path.join(__dirname, 'data.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
